2c621ae49689348d1b0537efb8cc7f9e7dac32f9
DarrylHellams <31867675+DarrylHellams@users.noreply.github.com>
2018-09-10 17:11:48 -0400

    Created Movement For AppleTree

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
+    //Prefab for instantiating apples
+    public GameObject apple;
+
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTrees turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
 	// Use this for initialization
 	void Start () {
-		
+		//Dropping apples every second
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update() {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //Move left
+        }
+    }
+    void FixedUpdate() { 
+        //Changing Direction Randomly is now time-based because of FixedUpdate()
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //Change direction
+        }
 	}
 }
