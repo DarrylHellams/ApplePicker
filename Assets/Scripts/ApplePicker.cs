f7b9a4be8c724ef75435f70d032316ea53548d37
DarrylHellams <31867675+DarrylHellams@users.noreply.github.com>
2018-09-12 17:03:11 -0400

    Added Score System (ask professor for help on AppleDestroyed method)

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -9,16 +9,25 @@ public class ApplePicker : MonoBehaviour {
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
 
-	// Use this for initialization
-	void Start () {
-		for (int i=0; i<numBaskets; i++)
+    // Use this for initialization
+    void Start() {
+        for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(BasketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
         }
-	}
+        public void AppleDestroyed()
+        {
+            GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+
+            foreach (GameObject tGO in tAppleArray)
+            {
+                Destroy(tGO);
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
